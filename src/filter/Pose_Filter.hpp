f91dd513b48781fd6f3ef2850cb0747b37887184
chrissteffen98 <chrissteffen98@gmail.com>
2024-08-04 12:19:33 +0200

    Improved filter to average values from sensors.

--- src/filter/Pose_Filter.hpp
+++ src/filter/Pose_Filter.hpp
@@ -3,6 +3,7 @@
 
 #include "rodos.h"
 #include "matlib.h"
+#include "Datastruct.h"
 
 #include "../Sub_Recv_Object.hpp"
 
@@ -29,22 +30,26 @@ extern Topic<Vector3D_F> filterPositionTopic;
 class PoseFilter : StaticThread<> {
 private:
 
+    Vector3D_F processPositionCov_ = Vector3D_F(1, 1, 1);  //The covariance of the model. Usually the same in all axis'.
+    Vector3D_F processAttitudeCov_ = Vector3D_F(1, 1, 1);   //The covariance of the model. Usually the same in all axis'.
+
     Vector3D_F orpePositionCov_ = Vector3D_F(1, 1, 10);   //The covariance of orpes position estimation. Usually higher in the direction of the target satellite (simplified by saying its higher in the z axis) 
     Vector3D_F orpeAttitudeCov_ = Vector3D_F(1, 1, 1);    //The covariance of orpes attitude estimation.
 
-    Vector3D_F uwbPositionCov_ = Vector3D_F(10, 10, 10);  //The covariance of uwb radio based position estimations. Usually higher than orpe position estimation.
-
-    float attitudeNoiseCov_ = 1;   //The noise of the process for attitude. Same in all axis'.
-    float positionNoiseCov_ = 1;   //The noise fo the process for the position. Same in all axis'.
+    Vector3D_F uwbPositionCov_ = Vector3D_F(100, 100, 10);  //The covariance of uwb radio based position estimations. Usually higher than orpe position estimation.
 
 
-    SubscriberObjRecv<HTransform_F, PoseFilter> orpePoseRecv_;
+    SubscriberObjRecv<OrpeTelemetry, PoseFilter> orpePoseRecv_;
     SubscriberObjRecv<Vector3D_F, PoseFilter> uwbPosRecv_;
 
 
-    HTransform_F orpeData_;
+    Semaphore newDataSem_;
+
+    OrpeTelemetry orpeData_;
     Vector3D_F uwbData_;
 
+    bool orpeNewData_ = false;
+    bool uwbNewData_ = false;
 
     Quaternion_F attitude_;
     Vector3D_F position_;
@@ -52,7 +57,7 @@ private:
 
 public:
 
-    PoseFilter(Topic<HTransform_F>& orpePoseTopic, Topic<Vector3D_F>& uwbPositionTopic);
+    PoseFilter(Topic<OrpeTelemetry>& orpePoseTopic, Topic<Vector3D_F>& uwbPositionTopic);
 
 
     void init() override;
@@ -68,7 +73,7 @@ private:
 
     /// @brief Receives the pose estmiations from orpe from topic.
     /// @param orpeEst 
-    void orpeEstRecv(HTransform_F& orpeEst);
+    void orpeEstRecv(OrpeTelemetry& orpeEst);
     
     /// @brief Receives the position estimations from uwb from topic. 
     /// @param uwbEst 
